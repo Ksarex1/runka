3ecb8bc5361583e09b7242c84c6c36f16b30c54b
Roman Tkachev <romatromikfak@gmail.com>
2025-06-11 10:42:25 +0300

    whywezx

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -1,9 +1,50 @@
 <script setup>
+import {ref, watch} from 'vue'
+
+const isOpen = ref(false)
+const isTransitioning = ref(false)
+
+watch(isOpen, (newVal) => {
+  if (newVal) {
+    isTransitioning.value = true
+    document.body.style.overflow = 'hidden'
+  } else {
+    setTimeout(() => {
+      isTransitioning.value = false
+    }, 300) // Длительность transition
+    document.body.style.overflow = ''
+  }
+})
+
 
 </script>
 
 <template>
   <section class="flex flex-col">
+    <div v-if="isOpen || isTransitioning"
+         class="max-[1400px]:flex flex-col bg-white p-4 absolute top-0 right-0 z-105 px-15 lg:w-2/6 items-end h-screen
+            transition-transform duration-300 ease-in-out transform max-lg:w-4/6"
+         :class="isOpen ? 'translate-x-0' : 'translate-x-full'">
+
+      <svg xmlns="http://www.w3.org/2000/svg"
+           fill="none"
+           viewBox="0 0 24 24"
+           stroke="#005DFF"
+           class="w-8 h-8 mb-3 " @click="isOpen = false">
+        <path stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12" />
+      </svg>
+
+      <ul class="flex flex-col gap-4 text-end border-t border-gray-400 w-full pt-5">
+        <li>О нас</li>
+        <li>События</li>
+        <li>Продукты</li>
+        <li>Услуги</li>
+        <li>Контакты</li>
+      </ul>
+    </div>
     <div class="bg-primary w-full flex justify-center">
       <div class="container py-1.5">
 
@@ -15,23 +56,23 @@
 
           <div class="flex gap-8">
             <div class="flex gap-6">
-          <span class="flex items-center text-sm">
+          <span class="max-[460px]:hidden flex items-center text-sm">
             <img src="../assets/img/Ringer.svg" alt="phone" class="mr-1.5">
             <p>+7 (963) 232-82-23</p>
           </span>
 
-              <span class="flex items-center text-sm">
+              <span class="sm:flex items-center text-sm hidden">
             <img src="../assets/img/Email.svg" alt="email" class="mr-1.5">
             <p>info@runa-s.ru</p>
           </span>
 
-              <span class="flex items-center text-sm">
+              <span class="xl:flex items-center text-sm hidden">
             <img src="../assets/img/Location.svg" alt="phone" class="mr-1.5">
             <p>г. Н. Новгород, ул. Нестерова, дом 9, офис 804</p>
           </span>
             </div>
 
-            <div class="custom-select relative">
+            <div class="custom-select relative sm:flex items-center hidden">
               <select class="bg-white text-primary-black w-30 appearance-none pl-7 text-sm">
                 <option value="">русский</option>
                 <option value="">english</option>
@@ -40,8 +81,14 @@
           </div>
 
           <div class="gap-8 flex text-sm ">
-            <button class="shadow-lg bg-white text-primary-black font-semibold px-6.25 py-1 rounded-md cursor-pointer hover:bg-gray-300 transition">Войти</button>
-            <button class="shadow-lg text-white px-6.25 py-1 font-semibold rounded-md border-white border cursor-pointer hover:text-primary-black hover:bg-white transition">Регистрация</button>
+            <button
+                class="shadow-lg bg-white text-primary-black font-semibold px-6.25 py-1 rounded-md cursor-pointer hover:bg-gray-300 transition">
+              Войти
+            </button>
+            <button
+                class="shadow-lg hidden lg:block text-white px-6.25 py-1 font-semibold rounded-md border-white border cursor-pointer hover:text-primary-black hover:bg-white transition">
+              Регистрация
+            </button>
           </div>
 
         </div>
@@ -52,12 +99,15 @@
     <div class=" w-full flex justify-center shadow-xl/20">
       <div class="container py-1.5 flex justify-between py-4">
 
+
+
         <div>
           <img src="../assets/img/logo.png" alt="logo">
         </div>
 
         <div class="flex items-center">
-          <ul class="flex gap-9">
+
+          <ul class="main flex gap-9 max-[1400px]:hidden">
             <li class="group relative">
               <p>О нас</p>
               <ul class="absolute hidden group-hover:flex flex-col gap-2 bg-white text-black p-2 rounded shadow-lg mt-2 w-35 z-50">
@@ -114,20 +164,34 @@
           </ul>
         </div>
 
-        <div class="search rounded-xl border" style="position: relative">
-          <input type="text" placeholder="Поиск..." class="rounded-xl py-0" />
+        <div class="flex gap-10">
+          <div class="search rounded-xl border max-sm:hidden block" style="position: relative">
+            <input type="text" placeholder="Поиск..." class="rounded-xl py-0"/>
+          </div>
+          <button class="max-[1400px]:block hidden" @click="isOpen=!isOpen">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"/>
+            </svg>
+          </button>
         </div>
+
       </div>
 
     </div>
-
+    <div v-if="isOpen"
+         class="fixed inset-0 z-100 bg-black/55 backdrop-blur-[2px]"
+         @click="isOpen = false">
+    </div>
   </section>
 
 
 </template>
 
 <style>
-
+body{
+  position: relative;
+}
 .custom-select::after {
   content: '';
   position: absolute;
@@ -137,32 +201,37 @@
   width: 1.3rem;
   height: 1.3rem;
   background-image: url('../assets/img/Sort.svg');
-  z-index: 120;
+  z-index: 100;
   background-size: contain;
   background-repeat: no-repeat;
   pointer-events: none;
 }
-p{
+
+p {
   font-weight: 500;
 }
-button{
+
+button {
   letter-spacing: 0.03em;
 }
 
-li p{
+li p {
   color: #1B1B1B;
   font-weight: 600;
   font-size: 14px;
 }
+
 .search {
   position: relative;
   border: 1px solid #3e3e3e;
   overflow: hidden;
   font-size: 12px;
 }
+
 input::placeholder {
   font-size: 12px;
 }
+
 .search input {
   padding-left: 40px;
   padding-right: 1rem;
@@ -187,20 +256,23 @@ input::placeholder {
   background-repeat: no-repeat;
   background-position: center;
   pointer-events: none;
-  z-index: 120;
+  z-index: 100;
 }
-select{
+
+select {
   border: 2px solid #003693;
 }
-select:focus{
+
+select:focus {
   border: 2px solid #3e3e3e;
   outline: none;
 }
+
 li {
   position: relative;
 }
 
-li::after {
+.main li::after {
   content: ''; /* Пустое содержимое */
   position: absolute;
   right: -1.1rem;
@@ -214,7 +286,8 @@ li::after {
   background-position: center;
   pointer-events: none;
 }
-ul p{
+
+ul p {
   cursor: pointer;
 }
 </style>
--- vue/src/components/Welcome.vue
+++ vue/src/components/Welcome.vue
@@ -5,22 +5,30 @@
 <template>
   <section class="h-full py-15">
     <div class="container">
-      <div class="grid grid-cols-12 gap-6 flex items-center">
-        <div class="col-span-5">
-          <h1 class="mb-4 text-[50px]">Точные решения<br> для эффективной бухгалтерии</h1>
-          <p class="text-[18px] text-primary3 mb-15">Мы решаем самые трудные задачи автоматизации в государственных учреждениях самыми простыми способами. Мы готовы взять на себя решение любых задач от самых простых и до самых сложных</p>
-          <div class="flex gap-5">
+      <div class="flex flex-col-reverse lg:grid lg:grid-cols-12 md:gap-6 md:items-center">
+
+        <div class="md:col-span-5 flex flex-col items-center text-center md:items-start md:text-left mt-8 md:mt-0">
+          <h1 class="mb-4 text-[32px] sm:text-[40px] md:text-[50px] leading-tight max-md:leading-10">
+            Точные решения<br> для эффективной бухгалтерии
+          </h1>
+          <p class="text-[16px] md:text-[18px] text-primary3 mb-6 md:mb-15">
+            Мы решаем самые трудные задачи автоматизации в государственных учреждениях самыми простыми способами. Мы готовы взять на себя решение любых задач от самых простых и до самых сложных
+          </p>
+          <div class="flex flex-col sm:flex-row gap-4 md:gap-5">
             <button class="px-10 bg-primary2 text-white rounded-xl py-2 cursor-pointer hover:bg-primary transition">Подробнее</button>
-            <button class="px-10 bg-white text-primary3 rounded-xl py-2 cursor-pointer border hover:bg-gray-200 transition">Подробнее</button>
+            <button class="px-10 bg-white text-primary3 rounded-xl py-2 cursor-pointer border hover:bg-gray-200 transition block max-md:hidden">Подробнее</button>
           </div>
         </div>
-        <div class="col-span-7 ">
-          <img src="../assets/img/welcomeIMg.png" alt="welcome" class="w-full h-auto">
+
+        <div class="md:col-span-7 flex justify-center">
+          <img src="../assets/img/welcomeIMg.png" alt="welcome" class="max-md:w-45 h-auto min-md:w-90 min-lg:w-full ">
         </div>
+
       </div>
     </div>
   </section>
 </template>
+
 
 
 <style scoped>
@@ -42,5 +50,10 @@ section{
   background-image: url('../assets/img/welcome.png');
   background-repeat: no-repeat;
   background-size: cover
+}
+@media (max-width: 620px) {
+  h1{
+    line-height: 40px;
+  }
 }
 </style>
